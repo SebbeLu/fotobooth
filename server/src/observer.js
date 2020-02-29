7613acabc9f429ff7060a3c276471db23ced1f5f
DarkOverride <sb.bs@mail.de>
2020-02-29 08:10:47 +0100

    added full path for sharp

--- server/src/observer.js
+++ server/src/observer.js
@@ -1,13 +1,14 @@
 const chokidar = require('chokidar')
 const sharp = require('sharp')
 const config = require('./config.json')
+const path = require('path')
 
 var methods = {}
 
 methods.watchUpload = function () {
-  chokidar.watch(config.uploaded).on('add', (event, path) => {
-    console.log(event, path)
-    sharp(path).resize({ width: 1080 }).toFile(config.imageFolder)
+  chokidar.watch(config.upload).on('add', (imagePath, event) => {
+    console.log(imagePath, path.basename(imagePath), event)
+    sharp(imagePath).resize({ width: 1080 }).toFile(config.imageFolder + '/' + path.basename(imagePath))
   })
 }
 module.exports = methods
