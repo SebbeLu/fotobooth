c8a986ad07356f428bd7104787329925fc305264
DarkOverride <sb.bs@mail.de>
2020-03-01 10:26:33 +0100

    client add socket.io-client

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,7 +4,12 @@ import Router from 'vue-router'
 import Start from '@/components/start'
 import Countdown from '@/components/countdown'
 import Gallery from '@/components/gallery'
+import socketio from 'socket.io-client'
+import VueSocketIO from 'vue-socket.io'
 
+export const SocketInstance = socketio('http://192.168.4.1:8081/')
+
+Vue.use(VueSocketIO, SocketInstance)
 Vue.use(Router)
 Vue.use(Buefy)
 
